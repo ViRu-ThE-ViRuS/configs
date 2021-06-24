0efd64d1d510acb9f606b3aed38003ae9274e95e
Viraat Chandra <viraat.chandra@hotmail.com>
2021-06-24 15:07:22 +0530

    remove bloat keymaps

--- nvim/lua/plug-config/neoscroll.lua
+++ nvim/lua/plug-config/neoscroll.lua
@@ -3,8 +3,6 @@ require('neoscroll').setup({ })
 local mappings = {}
 mappings['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '50'}}
 mappings['<C-d>'] = {'scroll', {'vim.wo.scroll', 'true', '50'}}
-mappings['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '50'}}
-mappings['<C-f>'] = {'scroll', {'vim.api.nvim_win_get_height(0)', 'true', '50'}}
 mappings['<C-y>'] = {'scroll', {'-0.10', 'false', '50'}}
 mappings['<C-e>'] = {'scroll', {'0.10', 'false', '50'}}
 mappings['{'] = {'scroll', {'-0.10', 'true', '50', nil}}
