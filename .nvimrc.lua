98de156b79a5b59ff33c52e11bce64e031c465f6
Viraat Chandra <viraat.chandra@hotmail.com>
2022-03-31 19:35:19 -0700

    add .nvimrc.lua: project local config

--- .nvimrc.lua
+++ .nvimrc.lua
@@ -4,7 +4,16 @@ vim.api.nvim_add_user_command('UpdateRepo', '!source update_repo.sh', {
     desc = 'Update repository with current config files'
 })
 
-vim.api.nvim_add_user_command('UpdateConfigs', '!source update_source.sh', {
+vim.api.nvim_add_user_command('UpdateConfigs', function()
+    vim.ui.select({'yes', 'no'}, {prompt = 'Update System Config>'},
+                  function(choice)
+        if choice == 'yes' then
+            vim.cmd [[ !source update_config.sh ]]
+        else
+            require("notify")('[config] update aborted', 'debug', {render='minimal'})
+        end
+    end)
+end, {
     bang = true,
     nargs = 0,
     desc = 'Update configs with current repository files'
