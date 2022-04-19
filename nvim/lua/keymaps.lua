1dbb17b02b3104832a9293633c430f8e6bc93b06
Viraat Chandra <viraat.chandra@hotmail.com>
2022-04-18 19:02:34 -0700

    remove old vim config, refactorings in keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,97 +8,47 @@ utils.map("v", ">", ">gv")
 utils.map({"n", "v", "o"}, "H", "^")
 utils.map({"n", "v", "o"}, "L", "$")
 
--- utility maps
-utils.map("i", "jj", "<esc>")
-utils.map("n", "U", "<c-r>")
-utils.map("n", ";", ":")
-utils.map("n", ":", ";")
-utils.map('n', 'Y', 'yy')
-utils.map("n", "<leader>1", misc.toggle_window)
-utils.map("n", "<leader>2", utils.random_colors, {silent = false})
-
--- toggles
-utils.map("n", "<leader>t1", "<cmd>if CWordHlToggle() | set hlsearch | endif<cr>")
-utils.map("n", "<leader>t2", misc.toggle_global_statusline)
-utils.map("n", "<leader>t3", misc.toggle_thicc_separators)
-utils.map("n", "<leader>t4", misc.toggle_spellings)
-
--- folds
-utils.map("n", "<space>", "za")
-
--- paste yanked
-utils.map({"n", "o"}, "-", '"0p')
-utils.map({"n", "o"}, "_", '"0P')
-
--- misc
-utils.map("n", "/", "ms/")                                  -- mark search start
-utils.map("n", "?", "ms?")                                  -- mark search start
-utils.map("v", "&", ":&&<cr>")                              -- substitutions
-utils.map("v", ".", ":normal! .<cr>")                       -- . motions
-utils.map("v", "@", ":normal! @")                           -- macros
-utils.map("n", "ss", "s")                                   -- substitute mode
-utils.map({"n", "v"}, "<c-b>", "<nop>")                     -- disable <c-b>
-utils.map("n", "gp", "`[v`]")                               -- last paste
-utils.map("n", "p", "p`[v`]=")                              -- autoformat paste
-utils.map("n", "P", "P`[v`]=")                              -- autoformat paste
-
 -- delete without yank
 -- NOTE(vir): <c-v> in insert mode, to get key code
 utils.map({"n", "v"}, "x", '"_d', {noremap=false})
 utils.map({"n", "v"}, "X", '"_dd', {noremap=false})
 utils.map({"n", "v"}, "<a-bs>", '"_dl', {noremap=false})
 
--- buffer resizing
-utils.map("n", "<m-j>", "<cmd>resize +2<cr>")
-utils.map("n", "<m-k>", "<cmd>resize -2<cr>")
-utils.map("n", "<m-h>", "<cmd>vertical resize -2<cr>")
-utils.map("n", "<m-l>", "<cmd>vertical resize +2<cr>")
+-- paste yanked
+utils.map({"n", "o"}, "-", '"0p')
+utils.map({"n", "o"}, "_", '"0P')
+
+-- misc
+utils.map("n", ";", ":")                                    -- swaperoo
+utils.map("n", ":", ";")                                    -- swaperoo
+utils.map("i", "jj", "<esc>")                               -- home-row escape
+utils.map("n", "U", "<c-r>")                                -- undo
+utils.map('n', 'Y', 'yy')                                   -- yank full line
+utils.map("n", "<space>", "za")                             -- toggle folds
+utils.map("n", "/", "ms/")                                  -- mark search start
+utils.map("n", "?", "ms?")                                  -- mark search start
+utils.map("v", "&", ":&&<cr>")                              -- substitutions
+utils.map("v", ".", ":normal! .<cr>")                       -- . motions
+utils.map("v", "@", ":normal! @")                           -- macros
+utils.map("n", "ss", "s")                                   -- substitute mode
+utils.map("n", "gp", "`[v`]")                               -- last paste
+utils.map("n", "p", "p`[v`]=")                              -- autoformat paste
+utils.map("n", "P", "P`[v`]=")                              -- autoformat Paste
+utils.map({"n", "v"}, "<c-b>", "<nop>")                     -- disable <c-b>
+
+-- disable command history, EX mode
+utils.map({"n", "v"}, "q:", "<nop>")
+utils.map({"n", "v"}, "Q", "<cmd>normal %<cr>")
+
+-- mouse scrolling
+utils.map("n", "<ScrollWheelUp>", "<c-y>")
+utils.map("n", "<ScrollWheelDown>", "<c-e>")
 
 -- sane speed scrolling
 utils.map({"n", "v"}, "{", "4k")
 utils.map({"n", "v"}, "}", "4j")
 utils.map({"n", "v"}, "<c-u>", "20kzz")
 utils.map({"n", "v"}, "<c-d>", "20jzz")
-
--- mouse scrolling
-utils.map("n", "<ScrollWheelUp>", "<c-y>")
-utils.map("n", "<ScrollWheelDown>", "<c-e>")
-
--- qflist
-utils.map("n", "[q", "<cmd>try | cprev | catch | clast | catch | endtry<cr>", {silent=true})
-utils.map("n", "]q", "<cmd>try | cnext | catch | cfirst | catch | endtry<cr>", {silent=true})
-utils.map("n", "<leader>Q", misc.toggle_qflist)
-
--- buffer navigation
--- NOTE(vir): bufdelete loaded after BufReadPost, causes error on fresh start
-utils.map("n", "<a-[>", "<cmd>bprev<cr>")
-utils.map("n", "<a-]>", "<cmd>bnext<cr>")
-utils.map("n", "<bs>", '<c-^>zz')
-utils.map("n", "<leader>q", function() require('bufdelete').bufdelete(0, true) end)
-utils.map("n", "<c-w><c-l>", "<cmd>cclose<cr> <cmd>pclose<cr> <cmd>lclose<cr> <cmd>tabclose<cr>", {silent=true})
-
--- terminal navigation
-utils.map("t", "<esc>", "<c-\\><c-n>")
-utils.map("t", "<c-h>", "<c-\\><c-w>h")
-utils.map("t", "<c-j>", "<c-\\><c-w>j")
-utils.map("t", "<c-k>", "<c-\\><c-w>k")
-utils.map("t", "<c-l>", "<c-\\><c-w>l")
-
--- terminal run config maps
-utils.map("n", "<leader>cA", terminal.run_target_command)
-utils.map("n", "<leader>ca", terminal.run_previous_command)
-utils.map("n", "<leader>cS", terminal.set_target)
-utils.map("n", "<leader>cs", function() terminal.toggle_target(false) end)
-
--- hardcore mode
-utils.map({"n", "i"}, "<up>", "<nop>")
-utils.map({"n", "i"}, "<down>", "<nop>")
-utils.map({"n", "i"}, "<left>", "<nop>")
-utils.map({"n", "i"}, "<right>", "<nop>")
-
--- disable command history, EX mode
-utils.map({"n", "v"}, "q:", "<nop>")
-utils.map({"n", "v"}, "Q", "<cmd>normal %<cr>")
 
 -- fixing that stupid typo when trying to [save]exit
 vim.cmd [[
@@ -110,12 +60,54 @@ vim.cmd [[
     cnoreabbrev <expr> ;w    ((getcmdtype()  is# ':' && getcmdline() is# ';w')?('w'):(';w'))
 ]]
 
--- new terminal
+-- hardcore mode
+utils.map({"n", "i"}, "<up>", "<nop>")
+utils.map({"n", "i"}, "<down>", "<nop>")
+utils.map({"n", "i"}, "<left>", "<nop>")
+utils.map({"n", "i"}, "<right>", "<nop>")
+
+-- toggles
+utils.map("n", "<leader>1", misc.toggle_window)
+utils.map("n", "<leader>2", utils.random_colors, {silent = false})
+utils.map("n", "<leader>t1", "<cmd>if CWordHlToggle() | set hlsearch | endif<cr>")
+utils.map("n", "<leader>t2", misc.toggle_global_statusline)
+utils.map("n", "<leader>t3", misc.toggle_thicc_separators)
+utils.map("n", "<leader>t4", misc.toggle_spellings)
+
+-- qflist navigation & toggle
+utils.map("n", "[q", "<cmd>try | cprev | catch | clast | catch | endtry<cr>", {silent=true})
+utils.map("n", "]q", "<cmd>try | cnext | catch | cfirst | catch | endtry<cr>", {silent=true})
+utils.map("n", "<leader>Q", misc.toggle_qflist)
+
+-- buffer resizing
+utils.map("n", "<m-j>", "<cmd>resize +2<cr>")
+utils.map("n", "<m-k>", "<cmd>resize -2<cr>")
+utils.map("n", "<m-h>", "<cmd>vertical resize -2<cr>")
+utils.map("n", "<m-l>", "<cmd>vertical resize +2<cr>")
+
+-- buffer navigation
+-- NOTE(vir): bufdelete loaded after BufReadPost, causes error on fresh start
+utils.map("n", "<bs>", '<c-^>zz')
+utils.map("n", "<a-[>", "<cmd>bprev<cr>")
+utils.map("n", "<a-]>", "<cmd>bnext<cr>")
+utils.map("n", "<leader>q", function() require('bufdelete').bufdelete(0, true) end)
+utils.map("n", "<c-w><c-l>", "<cmd>cclose<cr> <cmd>pclose<cr> <cmd>lclose<cr> <cmd>tabclose<cr>", {silent=true})
+
+-- terminal navigation
+utils.map("t", "<esc>", "<c-\\><c-n>")
+utils.map("t", "<c-h>", "<c-\\><c-w>h")
+utils.map("t", "<c-j>", "<c-\\><c-w>j")
+utils.map("t", "<c-k>", "<c-\\><c-w>k")
+utils.map("t", "<c-l>", "<c-\\><c-w>l")
+
+-- terminal setup
 utils.map("n", "<leader>s", "<cmd>vsp term://" .. vim.o.shell .. "<cr>")
+utils.map("n", "<leader>cA", terminal.run_target_command)
+utils.map("n", "<leader>ca", terminal.run_previous_command)
+utils.map("n", "<leader>cS", terminal.set_target)
+utils.map("n", "<leader>cs", terminal.toggle_target)
 
--- NOTE(vir): tabular
+-- NOTE(vir): external plugins (couldnt find a better place)
 utils.map("v", "<leader>=", ":Tab /")
-
--- NOTE(vir): fugitive
 utils.map("n", "<leader>gD", "<cmd>G! difftool<cr>")
 
