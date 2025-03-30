d76d3c92e1b989a0cad6d78a0860e5c8bd57639a
Viraat Chandra <viraat.chandra@hotmail.com>
2025-03-30 15:10:04 -0700

    updates

--- nvim/lua/plug-config/flash.lua
+++ nvim/lua/plug-config/flash.lua
@@ -4,8 +4,9 @@ return {
   opts = { modes = { search = { enabled = true } } },
   init = function()
     local utils = require("utils")
-    utils.map("n", "sf", function() require('flash').jump() end, { desc = "Flash" })
-    utils.map("n", "sF", function() require('flash').treesitter() end, { desc = "Flash" })
+    utils.map({"n", "x", "o"}, "sf", function() require('flash').jump() end)
+    utils.map({"n", "x", "o"}, "sF", function() require('flash').treesitter_search() end)
+    utils.map({"x", "o"}, "r", function() require('flash').remote() end)
     utils.add_command("[MISC] Toggle Flash Search", function() require("flash").toggle() end, { add_custom = true })
   end
 }
